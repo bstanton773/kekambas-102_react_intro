da42e5d0727e2207d25702727caddf98926d4ef1
Brian <bstanton773@gmail.com>
2022-11-07 15:35:27 -0600

    Add buttons that have their own state

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
+import Button from "./components/Button";
 import Navbar from "./components/Navbar";
 
 
 function App() {
     let myName = 'Brian';
     let myCity = 'Chicago';
+    let buttons = [
+        {color: 'primary', step: 1},
+        {color: 'secondary', step: 10},
+        {color: 'success', step: 100},
+        {color: 'danger', step: 1000},
+    ]
     return (
         <>
             <Navbar name={myName} test={123} city={myCity}/>
             <div className="container">
                 <h1>Hello World</h1>
                 <h4>Good Bye World</h4>
+                {buttons.map((button, idx) => <Button color={button.color} step={button.step} key={idx}/>)}
             </div>
         </>
     )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 
 export default function Navbar(props) {
-    // console.log(props);
-    // console.log(typeof props);
     return (
         <nav className="navbar navbar-dark bg-dark">
             <div className="container-fluid">
